d84ec673af0f9e25ae4d06c3ac3c2b8f41a6438a
hillel <hillel95med@gmail.com>
2024-04-08 09:01:12 +0300

    fix some sonar issues and remove jetbrain annotations

--- src/main/java/io/github/hillelmed/jenkins/client/domain/common/IntegerResponse.java
+++ src/main/java/io/github/hillelmed/jenkins/client/domain/common/IntegerResponse.java
@@ -1,14 +1,11 @@
 package io.github.hillelmed.jenkins.client.domain.common;
 
 import lombok.*;
-import org.jetbrains.annotations.*;
-
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class IntegerResponse {
 
-    @NotNull
     private Integer values;
 }
